9149f7c7901e93b7923af81867a5b83a3288c6c7
Jonathan Olsen <jonathanjolsen@gmail.com>
2023-06-29 17:19:02 -0700

    getSingleUser Route Created

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -2,9 +2,14 @@ const router = require('express').Router();
 
 const {
     getUsers,
+    getSingleUser,
 } = require('../../controllers/userController')
 
 //Get route for all users
 router.route('/').get(getUsers);
+
+router
+    .route('/:userId')
+    .get(getSingleUser);
 
 module.exports = router;
