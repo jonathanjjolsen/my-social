b1db971942aefb57b4f20949daa1b286a834b94d
Jonathan Olsen <jonathanjolsen@gmail.com>
2023-07-01 20:33:32 -0700

    Added some of the Thought.js Logic

---
+++ models/Thought.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose');
+
+const thoughtModel = new Schema(
+    {
+        thoughtText:{
+            type: String,
+            required: true,
+            maxlength: 280,
+            minlenght: 1,
+        },
+        //Creates a time stamp with a default value of the current date/time
+        createdAt:{
+            type: Date,
+            default: Date.now,
+            get: timestamp => new Date(timestamp).toLocaleString,
+        },
+        username:{
+            type: String,
+            required: true,
+        },
+        reactions:[reactionModel],
+    },
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true,
+        },
+        id: false,
+    }
+)
+
+const Thought = model('Thought', thoughtModel);
+
+module.exports = Thought;
