2ea194b79c5a9f27553eef82010bf1c84496efaa
Jonathan Olsen <jonathanjolsen@gmail.com>
2023-07-01 20:53:38 -0700

    Formatting

--- models/User.js
+++ models/User.js
@@ -4,27 +4,31 @@ const { Schema, model } = require('mongoose');
 
 const userModel = new Schema(
     {
-       username: {
-        type: String,
-        unique: true,
-        required: true,
-        trim: true,
-       },
-        email: {
-        type: String,
-        unique: true,
-        required: true,
-        validate: {
-            validator: function(v) {
-              return /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6})$/.test(v);
-            }}
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
         },
+
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            validate: {
+                validator: function (v) {
+                    return /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6})$/.test(v);
+                }
+            }
+        },
+
         thoughts: [
             {
                 type: Schema.Types.ObjectId,
                 ref: 'Thought',
             }
         ],
+        
         friends: [
             {
                 type: Schema.Types.ObjectId,
@@ -33,14 +37,14 @@ const userModel = new Schema(
         ]
     },
     {
-        toJson:{
+        toJson: {
             virtuals: true
         },
     }
 );
 
 //Retrieves the user's friends array by length and creates a virtual number, not to be stored in the DB.
-userModel.virtual('friendCount').get(function(){
+userModel.virtual('friendCount').get(function () {
     return this.friends.length;
 })
 
