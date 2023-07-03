67736d28c4cddd6819d2fe16d571389f527ddc70
Jonathan Olsen <jonathanjolsen@gmail.com>
2023-07-03 06:20:11 -0700

    Removed unneccesary package

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,5 @@
 //Import mongoose for DB connection
 const mongoose = require('mongoose');
-require('dotenv').config();
 
 //Uses dotenv to connect to DB or statement for default route if variables are unavailable
 const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/mySocial';
