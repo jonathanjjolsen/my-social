bfa0a1165c771c383260c59b9a6abc54b230f83d
Jonathan Olsen <jonathanjolsen@gmail.com>
2023-07-10 16:06:04 -0700

    Added updateThought

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -2,27 +2,42 @@ const { Thought, User, Reaction } = require('../models');
 
 module.exports = {
     async getThoughts(req, res) {
-        try{
+        try {
             const thoughts = await Thought.find();
             res.json(thoughts);
         } catch (err) {
             res.status(500).json(err);
         }
     },
-    async getSingleThought(req,res){
-        try{
-            const thought = await Thought.findOne({ _id: req.params.thoughtId})
+    async getSingleThought(req, res) {
+        try {
+            const thought = await Thought.findOne({ _id: req.params.thoughtId })
             res.json(thought)
         } catch (err) {
             res.status(500).json(err);
         }
     },
     async addThought(req, res) {
-        try{
+        try {
             const thought = await Thought.create(req.body);
             res.status(201).json(thought);
         } catch (err) {
             res.status(500).json(err);
         }
     },
+    async updateThought(req, res) {
+        try {
+            const thought = await Thought.findByIdAndUpdate(
+                { _id: req.params.thoughtId },
+                { $set: req.body },
+                { runValidators: true, new: true },
+            );
+            if(!thought) {
+                res.status(404).json({ message: 'No thought with this ID.'})
+            }
+            res.json(thought)
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
 }
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -4,11 +4,13 @@ const {
     getThoughts,
     getSingleThought,
     addThought,
+    updateThought,
 } = require('../../controllers/thoughtsController')
 
 router.route('/').get(getThoughts).post(addThought)
 
 router.route('/:thoughtId')
     .get(getSingleThought)
+    .put(updateThought)
 
 module.exports = router;
